a12812166536ef41b3dd95f92d589e1125a82762
ringler0 <31633100+ringler0@users.noreply.github.com>
2017-11-27 23:37:09 -0300

    Update ContarVotos.php

--- Votos/ContarVotos.php
+++ Votos/ContarVotos.php
@@ -1,16 +1,20 @@
 <?php
   include("conex.inc");
  
-  $consulta = "SELECT AVG(puntuacion) as puntos,id_veterinaria FROM puntuacion GROUP BY id_veterinaria";
+  $consulta = "SELECT AVG(puntuacion) as puntos,id_veterinaria, veterinaria, lat, lng FROM puntuacion NATURAL JOIN veterinarias GROUP BY id_veterinaria";
   $respuesta = mysqli_query($db, $consulta);
   $salida = "[";
   while($fila = mysqli_fetch_object($respuesta)) {
     $punto = round($fila->puntos,1);
     $id = $fila->id_veterinaria;
+    $vete = $fila->veterinaria;
+    $lat = $fila->lat;
+    $lng = $fila->lng;
     if($salida != "[") { //si no es el 1er registro
         $salida .= ",";
     }
-    $salida .= '{"Puntos":"' .$punto. '","id":"'.$id.'"}';
+    $salida .= '{"Puntos":"'.$punto.'","id":"'.$id.'","vete":"'.$vete.'","lat":'.$lat.',"lng":'.$lng.'}';
+    
   }
   $salida .= "]";
  
